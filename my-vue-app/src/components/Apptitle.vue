3856c00b7704d8d1e1288a732e2c6bfbb42c55a4
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-28 01:21:57 +0100

    Esercizio completato, con aneddoto all'interno!

---
+++ my-vue-app/src/components/Apptitle.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<template>
+    <h1>Bisciabu</h1>
+    <h2>Pronuncia : BiŠciabù</h2>
+    <h2>Storia</h2>
+    <p>Bisciabu è una parola derivata in origine da un' altra parola priva di senso che è stata coniata da alcuni bulletti
+        del mio paese, in origine era "Bidabù" e come specificato in precedenza era una parola priva di senso, spesso
+        pronunciata come sostantivo goliardico per affermare la propria supremazia durante le marachelle.
+        Tuttavia, un gruppo di amici cambiò lievemente la struttura ed il senso della parola per darle una connotazione meno
+        infantile, più dialettale e comica, rendendola un'ottima parola per le freddure.
+        Così nacque Bisciabu, che si scrive rigorosamente senza accento sulla "U", e sta a significare "adesso sono problemi
+        tuoi", "fatti in c**o". Il metodo ideale di utilizzo è pronunciarla proprio al termine di una frase con un
+        avvenimento dal risvolto poco favorevole per il soggetto che l'ha pronunciata.</p>
+
+    <h2>Esempio (un pò estremo) :</h2>
+    <p> Leonardo :-"Ciao Gianlù, vedi domani non riesco a venire a calcetto, purtroppo mio padre è finito in un burrone ieri
+        sera con la macchina, di conseguenza passerò la nottata con lui in ospedale, sperando che si riprenda. "</p>
+<p>Gianluca :-"E Bisciabu Leonardo!"</p></template>
+
+<style scoped></style>
